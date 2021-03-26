f0a3b4e8226113d9d038f01b641928b3af63d0fb
Xuefeng Ding <xuefeng.ding.physics@gmail.com>
2021-03-26 07:59:58 -0400

    use external add

---
+++ tests/test.cc
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "executor.h"
+
+TEST (MyTest, FirstTest) { 
+    EXPECT_EQ (145, calculate())
+}
+
+TEST (MyTest, SecondTest) { 
+    EXPECT_EQ (190, calculate2())
+}
